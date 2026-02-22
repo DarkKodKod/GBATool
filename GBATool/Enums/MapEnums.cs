b4af8774a970e41ea46065ca30589ffba6bdb7b3
DarkKodKod <freinaud@gmail.com>
2026-02-22 19:00:39 +0100

    Added MapModel members.

--- GBATool/Enums/MapEnums.cs
+++ GBATool/Enums/MapEnums.cs
@@ -5,3 +5,19 @@ public enum MapType
     Regular = 0,
     Affine = 1
 }
+
+public enum BckgrRegularSize
+{
+    Regular32x32 = 0,
+    Regular64x32 = 1,
+    Regular32x64 = 2,
+    Regular64x64 = 3
+}
+
+public enum BckgrAffineSize
+{
+    Affine16x16 = 0,
+    Affine32x32 = 1,
+    Affine64x64 = 2,
+    Affine128x128 = 3
+}
--- GBATool/Models/MapModel.cs
+++ GBATool/Models/MapModel.cs
@@ -4,6 +4,16 @@ using System.Windows;
 
 namespace GBATool.Models;
 
+public class Tile
+{
+    public string ID { get; set; } = string.Empty;
+    public bool FlipHorizontal { get; set; }
+    public bool FlipVertical { get; set; }
+    public string PaletteID { get; set; } = string.Empty;
+    public int PaletteIndex { get; set; }
+    public string TileSetID { get; set; } = string.Empty;
+}
+
 public class MapModel : AFileModel
 {
     private const string _extensionKey = "extensionMaps";
@@ -22,5 +32,15 @@ public class MapModel : AFileModel
         }
     }
 
-    public MapType MapType { get; set; }
+    [TomlIgnore]
+    public const int RegularTileMax = 64 * 64;
+
+    public MapType MapType { get; set; } = MapType.Regular;
+    public Priority Priority { get; set; } = Priority.Highest;
+    public BckgrRegularSize BckgrRegularSize { get; set; } = BckgrRegularSize.Regular32x32;
+    public BckgrAffineSize BckgrAffineSize { get; set; } = BckgrAffineSize.Affine16x16;
+    public Tile[] RegularTiles { get; set; } = new Tile[RegularTileMax];
+    public Tile[] AffineTiles { get; set; } = new Tile[RegularTileMax * 2];
+    public bool EnableMosaic { get; set; }
+    public bool AffineWrapping { get; set; }
 }
