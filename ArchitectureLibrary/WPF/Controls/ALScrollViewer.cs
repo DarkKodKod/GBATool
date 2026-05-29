7e20e11bfd51e6d727236da2b5c28cd2f840f540
Felipe Reinaud <freinaud@gmail.com>
2026-05-29 09:19:15 +0200

    New custom control
    Added custom control from the ScrollViewer to disable the mouse wheel.

---
+++ ArchitectureLibrary/WPF/Controls/ALScrollViewer.cs
@@ -0,0 +1,31 @@
+﻿namespace ArchitectureLibrary.WPF.Controls;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+public class ALScrollViewer : ScrollViewer
+{
+    public static readonly DependencyProperty DisableMouseWheelProperty =
+        DependencyProperty.RegisterAttached(
+            "DisableMouseWheel", 
+            typeof(bool), 
+            typeof(ALScrollViewer), 
+            new PropertyMetadata(default(bool)));
+
+    public bool DisableMouseWheel
+    {
+        get => (bool)GetValue(DisableMouseWheelProperty);
+        set => SetValue(DisableMouseWheelProperty, value);
+    }
+
+    protected override void OnMouseWheel(MouseWheelEventArgs e)
+    {
+        if (DisableMouseWheel)
+        {
+            return;
+        }
+
+        base.OnMouseWheel(e);
+    }
+}
